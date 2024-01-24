9217b4d1af8e69b252ec0e08406602b9f0348b83
Your Name <s6330613008@phuket.psu.ac.th>
2024-01-24 10:15:01 +0700

    kk

--- app/config/db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports ={
-    HOST: "buav5hcfj3t7smkqt8yt-mysql.services.clever-cloud.com",
-    USER: "ugbn3govfxsg1h7z",
-    PASSWORD: "UfKVFv75aZJrqKiqZC5M",
-    DB: "buav5hcfj3t7smkqt8yt"
-};
