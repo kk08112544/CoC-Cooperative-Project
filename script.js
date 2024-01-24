87dec61492ac2228a861177717002f60dd621c4f
Your Name <s6330613008@phuket.psu.ac.th>
2024-01-24 10:17:59 +0700

    suwara

--- script.js
+++
@@ -1,9 +0,0 @@
-const currentDate = new Date();
-
-const year = currentDate.getFullYear();
-const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based, so add 1
-const day = currentDate.getDate().toString().padStart(2, '0');
-
-const formattedDate = `${year}-${month}-${day}`;
-
-console.log(formattedDate);
